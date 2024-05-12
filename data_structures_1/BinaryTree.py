86018d3a0b77e919f41cf9c5b0333560251002de
Ezequiel <ezequiel.maranda@gmail.com>
2024-05-12 08:58:18 -0300

    BinaryTree updated

--- data_structures_1/BinaryTree.py
+++ data_structures_1/BinaryTree.py
@@ -5,13 +5,44 @@ class BinaryTree:
         self.right = None
     
     def agregar(self, data):
-        pass
+        if data == self.data:
+            return
+        if data < self.data:
+            if self.left:
+                self.left.agregar(data)
+            else:
+                self.left = BinaryTree(data)
+        else:
+            if self.right:
+                self.right.agregar(data)
+            else:
+                self.right = BinaryTree(data)
 
+   
+    def listar(self):
+        datos = []
+        #agrego datos de la rama izquierda
+        if self.left:
+            datos += self.left.listar()
+        #agrego el dato del nodo actual
+        datos.append(self.data)
+        #agrego datos de la rama derecha
+        if self.right:
+            datos += self.right.listar()
+        return datos
+     
     def buscar(self, data):
         pass
 
     def borrar(self, data):
         pass
 
-    def listar(self):
-        pass
+
+if __name__ == '__main__':
+    bt = BinaryTree(8)
+    print("esto es coso")
+    bt.agregar(2)
+    bt.agregar(1)
+    bt.agregar(14)
+    datos = bt.listar()
+    print(datos)
