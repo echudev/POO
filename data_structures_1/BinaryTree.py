686d1039abd1c5fde68a72abe56b2f8732e50a66
Ezequiel <ezequiel.maranda@gmail.com>
2024-05-11 23:54:57 -0300

    BinaryTree added

---
+++ data_structures_1/BinaryTree.py
@@ -0,0 +1,17 @@
+class BinaryTree:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def agregar(self, data):
+        pass
+
+    def buscar(self, data):
+        pass
+
+    def borrar(self, data):
+        pass
+
+    def listar(self):
+        pass
