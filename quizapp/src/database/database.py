5d0ab689543940c585de63389c18484a20ede64c
Ezequiel <ezequiel.maranda@gmail.com>
2024-06-19 18:59:25 -0300

    database updated

--- quizapp/src/database/database.py
+++ quizapp/src/database/database.py
@@ -1,10 +1,8 @@
 import sqlite3
 
-preguntas_db = './db/preguntas.db'
-
-def run_query(query, parameters = ()):
+def run_query(query, db, parameters = ()):
     # usando 'with' se cierra automáticamente la conexión luego de ejecutar la consulta
-    with sqlite3.connect(preguntas_db) as conn:
+    with sqlite3.connect(db) as conn:
         cursor = conn.cursor()
         result = cursor.execute(query, parameters)
         conn.commit()
