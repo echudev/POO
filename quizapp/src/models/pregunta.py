38b59abdf12d7244ef4aa560b64443bdc6d1afac
Ezequiel <ezequiel.maranda@gmail.com>
2024-06-19 19:38:49 -0300

    game class refactored

--- quizapp/src/models/pregunta.py
+++ quizapp/src/models/pregunta.py
@@ -1,5 +1,4 @@
 from models.respuesta import Respuesta
-from quizapp.src.database.db import run_query
 
 class Pregunta:
     def __init__(self, nivel: int, enunciado: str):
@@ -22,23 +21,3 @@ class Pregunta:
             
     def get_nivel(self):
             return self.__nivel
-    
-    def get_preguntas_respuestas(self):
-        query = 'SELECT * FROM Preguntas WHERE nivel_id = 1 ORDER BY RANDOM() LIMIT 3;'
-        rows = run_query(query)
-        for row in rows:  
-            # instancio la pregunta
-            enunciado, nivel = row[1], row[2]
-            nueva_pregunta = Pregunta(nivel, enunciado)
- 
-            # instancio las respuestas para la pregunta
-            query2 = 'SELECT * FROM Respuestas WHERE pregunta_id = ? ORDER BY RANDOM();'
-            rows2 = run_query(query2, (row[0],))
-            respuestas = []
-            for row_ in rows2:
-                 texto, correcta = row_[1], row_[2]
-                 nueva_respuesta = Respuesta(texto, correcta)
-                 respuestas.append(nueva_respuesta)
-            nueva_pregunta.set_respuestas(respuestas)
-            self.preguntas.append(nueva_pregunta)
-        return self.preguntas
