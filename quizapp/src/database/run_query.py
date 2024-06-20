59917cf056d1e02455ecf3ca8e0a573de210df1a
Ezequiel <ezequiel.maranda@gmail.com>
2024-06-20 05:44:28 -0300

    types fixed

--- quizapp/src/database/run_query.py
+++ quizapp/src/database/run_query.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def run_query(query, db, parameters = ()):
+def run_query(query: str, db: str, parameters = ()):
     # usando 'with' se cierra automáticamente la conexión luego de ejecutar la consulta
     with sqlite3.connect(db) as conn:
         cursor = conn.cursor()
